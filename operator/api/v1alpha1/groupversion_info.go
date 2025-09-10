abeaca42fe4f2e204e8b995e59ec64c05166ffdc
CatalinPredica <catalinpredica@gmail.com>
2025-09-10 19:40:29 +0300

    add operator

--- operator/api/v1alpha1/groupversion_info.go
+++ operator/api/v1alpha1/groupversion_info.go
@@ -1,24 +1,24 @@
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
-// GroupName is the API group for the K8sAssistant CRD
 const GroupName = "k8s-assistant.io"
 
-// SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
+var (
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
 
-// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-var SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
 
-// AddToScheme adds all types of this clientset into the given scheme.
-var AddToScheme = SchemeBuilder.AddToScheme
+	// AddToScheme adds all types of this clientset into the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
 
+// Register the type with the SchemeBuilder
 func init() {
-	// Register the group version with the global scheme
-	metav1.AddToGroupVersion(scheme.Scheme, SchemeGroupVersion)
+	SchemeBuilder.Register(&K8sAssistant{}, &K8sAssistantList{})
 }
